fcbfbb277b7587bf864d0ec15eb11d7bdad18c9c
Роман Зиновец <r.zinovets1962@gmail.com>
2021-12-01 12:53:33 +0300

    ADDED functionality

---
+++ src/MyProject/Controllers/ArticlesController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace MyProject\Controllers;
+
+use MyProject\Exceptions\InvalidArgumentException;
+use MyProject\Exceptions\NotFoundException;
+use MyProject\Exceptions\UnauthorizedException;
+use MyProject\Models\Articles\Article;
+use MyProject\Models\Users\User;
+
+class ArticlesController extends AbstractController
+{
+    public function view(int $articleId): void
+    {
+        $article = Article::getById($articleId);
+
+        if ($article === null) {
+            throw new NotFoundException();
+        }
+
+        $this->view->renderHtml('articles/view.php', [
+            'article' => $article
+        ]);
+    }
+
+    public function edit(int $articleId)
+    {
+        $article = Article::getById($articleId);
+
+        if ($article === null) {
+            throw new NotFoundException();
+        }
+
+        if ($this->user === null) {
+            throw new UnauthorizedException();
+        }
+
+        if (!empty($_POST)) {
+            try {
+                $article->updateFromArray($_POST);
+            } catch (InvalidArgumentException $e) {
+                $this->view->renderHtml('articles/edit.php', ['error' => $e->getMessage(), 'article' => $article]);
+                return;
+            }
+
+            header('Location: /articles/' . $article->getId(), true, 302);
+            exit();
+        }
+
+        $this->view->renderHtml('articles/edit.php', ['article' => $article]);
+    }
+
+    public function add(): void
+    {
+        if ($this->user === null) {
+            throw new UnauthorizedException();
+        }
+
+        if (!empty($_POST)) {
+            try {
+                $article = Article::createFromArray($_POST, $this->user);
+            } catch (InvalidArgumentException $e) {
+                $this->view->renderHtml('articles/add.php', ['error' => $e->getMessage()]);
+                return;
+            }
+
+            header('Location: /articles/' . $article->getId(), true, 302);
+            exit();
+        }
+
+        $this->view->renderHtml('articles/add.php');
+    }
+}
---
+++ src/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'db' => [
+        'host' => '127.0.0.1',
+        'dbname' => 'my_project',
+        'user' => 'olopsman',
+        'password' => '12345678',
+        'port' => '3306'
+    ]
+];
